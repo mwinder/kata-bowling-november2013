34d7dc8bfa51c172e2c7a317fb056911abd41f6f
Matthew Winder <@mwinder>
2015-10-15 23:23:37 +0100

    init

---
+++ BowlingKata/DescribeBowlingGame.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Xunit;
+
+namespace BowlingKata
+{
+    public class DescribeBowlingGame
+    {
+        private Game game;
+
+        public DescribeBowlingGame()
+        {
+            game = new Game();
+        }
+
+        private void RollMany(int number, int pins)
+        {
+            for (int i = 0; i < number; i++)
+                game.Roll(pins);
+        }
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            game.Roll(10);
+        }
+
+        [Fact]
+        public void GutterGame()
+        {
+            RollMany(20, 0);
+
+            Assert.Equal(0, game.Score());
+        }
+
+        [Fact]
+        public void AllOnes()
+        {
+            RollMany(20, 1);
+
+            Assert.Equal(20, game.Score());
+        }
+
+        [Fact]
+        public void GameWithOneSpare()
+        {
+            RollSpare();
+            game.Roll(3);
+            RollMany(17, 0);
+
+            Assert.Equal(16, game.Score());
+        }
+
+        [Fact]
+        public void GameWithOneStrike()
+        {
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            RollMany(16, 0);
+
+            Assert.Equal(24, game.Score());
+        }
+
+        [Fact]
+        public void PerfectGame()
+        {
+            RollMany(12, 10);
+
+            Assert.Equal(300, game.Score());
+        }
+    }
+}
---
+++ BowlingKata/Game.cs
@@ -0,0 +1,67 @@
+﻿
+namespace BowlingKata
+{
+    public class Game
+    {
+        private int[] rolls = new int[21];
+        private int currentRoll = 0;
+
+        public void Roll(int pins)
+        {
+            rolls[currentRoll] = pins;
+            currentRoll++;
+        }
+
+        public int Score()
+        {
+            var score = 0;
+            var firstInFrame = 0;
+
+            for (int frame = 0; frame < 10; frame++)
+            {
+                if (IsStrike(firstInFrame))
+                {
+                    score += 10 + BonusForStrike(firstInFrame);
+                    firstInFrame += 1;
+                }
+                else if (IsSpare(firstInFrame))
+                {
+                    score += 10 + BonusForSpare(firstInFrame);
+                    firstInFrame += 2;
+                }
+                else
+                {
+                    score += TotalForFrame(firstInFrame);
+                    firstInFrame += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private int TotalForFrame(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1];
+        }
+
+        private bool IsStrike(int firstInFrame)
+        {
+            return rolls[firstInFrame] == 10;
+        }
+
+        private int BonusForStrike(int firstInFrame)
+        {
+            return rolls[firstInFrame + 1] + rolls[firstInFrame + 2];
+        }
+
+        private bool IsSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame] + rolls[firstInFrame + 1] == 10;
+        }
+
+        private int BonusForSpare(int firstInFrame)
+        {
+            return rolls[firstInFrame + 2];
+        }
+    }
+}
